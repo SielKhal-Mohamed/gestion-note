d9c9c4b0bc6925c37532b51539b826da3fdb024b
Mohamed Sielkhal <Mohamed Sielkhal@DESKTOP-VN64PSE>
2022-04-15 15:55:27 +0000

    compareTo

---
+++ gestion note/src/gestion_note/Main.java
@@ -0,0 +1,21 @@
+package gestion_note;
+
+public class Main {
+
+	public static void main(String[] args) {
+		etudiant ayoub = new etudiant(3214, "ayoub", 20);
+		etudiant hamid = new etudiant(2818, "hamid", 19);
+		etudiant simo = new etudiant(1010, "simo");
+		System.out.println(ayoub.toString());
+		System.out.println(hamid.toString());
+		System.out.println(simo.toString());
+		System.out.println(ayoub.equals(hamid));
+		System.out.println(ayoub.equals(simo));
+		System.out.println(ayoub.equals(ayoub));
+		System.out.println(ayoub.compareTo(ayoub));
+		System.out.println(hamid.compareTo(simo));
+		System.out.println(simo.compareTo(hamid));
+
+	}
+
+}
--- gestion note/src/gestion_note/etudiant.java
+++ gestion note/src/gestion_note/etudiant.java
@@ -1,6 +1,6 @@
 package gestion_note;
 
-public class etudiant {
+public class etudiant implements Comparable<etudiant> {
 	private int id;
 	private String nom;
 	private double note;
@@ -11,9 +11,29 @@ public class etudiant {
 		this.note = 10;
 	}
 
-	public String toString() {
-		return this.nom + this.note;
+	public etudiant(int id, String nom, double note) {
+		this.id = id;
+		this.nom = nom;
+		this.note = note;
+	}
 
+	public String toString() {
+		return this.nom + " : " + this.note;
+
+	}
+
+	public boolean equals(etudiant hamid) {
+		return this.id == hamid.id;
+
+	}
+
+	@Override
+	public int compareTo(etudiant hamid) {
+		if (this.note > hamid.note)
+			return 1;
+		if (this.note == hamid.note)
+			return 0;
+		return -1;
 	}
 
 }
