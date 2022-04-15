fbbd469121f1b93d520c4ee257db59d5b54f577f
Mohamed Sielkhal <Mohamed Sielkhal@DESKTOP-VN64PSE>
2022-04-15 14:38:50 +0000

    construction

--- gestion note/src/gestion_note/etudiant.java
+++ gestion note/src/gestion_note/etudiant.java
@@ -4,5 +4,10 @@ public class etudiant {
 	private int id;
 	private String nom;
 	private double note;
+	public Etudiant(int id,String nom) {
+		this.id =id;
+		this.nom =nom;
+		this.note =10;
+	}
 
 }
